59b8a9623a3e84fbe4175a8939d7e3492ee39bb2
turboflanagan <turboflanagan@gmail.com>
2015-12-14 16:26:48 -0500

    add layout view

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -20,7 +24,27 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Passport Configuration
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+
+
+//Mongoose
+mongoose.connect('mongodb://localhost:27017/coffee');
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var passport = require('passport');
+var Account = require('../models/account');
 var router = express.Router();
 
 /* GET home page. */
@@ -6,4 +8,30 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/register', function(req, res, next){
+	res.render('index');
+});
+
+router.post('/register', function(req, res, next){
+	Account.register(new Account(
+		{username: req.body.username}),
+	req.body.password,
+	function(error, account){
+		if(error){
+			console.log(error);
+			return res.render('index');
+		}else{
+			passport.authenticate('local')(req, res, function(){
+				res.redirect('/');
+				// passport.authenticate('local', { successRedirect: '/',
+    //                                                 failureRedirect: '/login' }));
+			});
+		};
+	});
+});
+
 module.exports = router;
+
+
+
+
