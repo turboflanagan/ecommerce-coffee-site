869d38f496df3db1bb2a8fd4e199d51d825dfc4d
turboflanagan <turboflanagan@gmail.com>
2015-12-14 16:50:05 -0500

    add login get route and page

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,10 @@ router.get('/register', function(req, res, next){
 	res.render('index');
 });
 
+router.get('/login', function(req, res, next){
+	res.render('login');
+});
+
 router.post('/register', function(req, res, next){
 	Account.register(new Account(
 		{username: req.body.username}),
