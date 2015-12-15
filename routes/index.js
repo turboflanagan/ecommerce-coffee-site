c65b4f8d8ffde57dccbb9f71011364e2c87cffed
turboflanagan <turboflanagan@gmail.com>
2015-12-15 15:39:45 -0500

    adjust index landing page

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	console.log(req.user);
+	res.render('index', { user : req.user });  // req.user gets created by passport
 });
 
+////////////////  REGISTER GET ROUTE  //////////////////////
 router.get('/register', function(req, res, next){
-	res.render('index');
+	res.render('register', {});
 });
 
-router.get('/login', function(req, res, next){
-	res.render('login');
-});
-
+////////////////  REGISTER POST ROUTE  //////////////////////
 router.post('/register', function(req, res, next){
 	Account.register(new Account(
 		{username: req.body.username}),
@@ -26,15 +25,38 @@ router.post('/register', function(req, res, next){
 			return res.render('index');
 		}else{
 			passport.authenticate('local')(req, res, function(){
+				req.session.username = req.body.username;
 				res.redirect('/');
-				// passport.authenticate('local', { successRedirect: '/',
-    //                                                 failureRedirect: '/login' }));
 			});
 		};
 	});
 });
 
+////////////////  LOGIN GET ROUTE  //////////////////////
+router.get('/login', function(req, res, next){
+	res.render('login');
+});
+
+router.post('/login', function(req, res, next){
+	req.session.username = req.body.username;
+	res.redirect('/');
+});
+
+
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
